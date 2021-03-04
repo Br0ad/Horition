63927d0eeb02b99391df31036aa06b2c1a12a1bc
pankiwi <arroliga.andrz1@gmail.com>
2021-03-04 17:39:41 -0600

    Fix erros

--- scripts/blocks/crafter/Industrial-tungsten-compressor.js
+++
@@ -1,6 +0,0 @@
-const Itt = extendContent(GenericCrafter, "Industrial-tungsten-compressor", {});
-const animation = new DrawAnimation();
-animation.frameCount = 10
-
-
-Itt.drawer = animation
--- scripts/blocks/crafter/Selenita-Reformer.js
+++
@@ -1,6 +0,0 @@
-const SR = extendContent(GenericCrafter, "Selenita-Reformer", {});
-
-const animation = new DrawAnimation();
-animation.frameCount = 5;
-
-SR.drawer = animation;
